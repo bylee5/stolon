6cc4717cf3ab91bb2462c5c4e92ad60b5898611a
bylee5 <bylee@bitnine.net>
2018-04-25 12:51:24 +0900

    Update example for stolon + kubernetes
    	* Add AgensGraph v1.3.0
    	* Add AgensGraph v1.3.1

---
+++ examples/docker_agensgraph_1.3.0/README.md
@@ -0,0 +1,5 @@
+# 빌드
+
+docker build -t stolon:master-ag1.3 ./
+
+
---
+++ examples/kubernetes+agensgraph_1.3.1/start.sh
@@ -0,0 +1,13 @@
+kubectl create -f role.yaml
+kubectl create -f role-binding.yaml
+kubectl create -f 00-default-admin-access.yaml
+kubectl create -f secret.yaml
+kubectl create -f ag_local-01.yaml
+kubectl create -f ag_local-02.yaml
+
+/home/bylee/dev/stolon-test/stolon/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init
+kubectl create -f stolon-sentinel.yaml
+
+#kubectl create -f stolon-keeper.yaml
+#kubectl create -f stolon-proxy.yaml
+#kubectl create -f stolon-proxy-service.yaml
